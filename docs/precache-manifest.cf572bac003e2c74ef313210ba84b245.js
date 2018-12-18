9e5e4f2f049654fbc79230e80968159fcddba82b
xavierdpt@gmail.com <xavierdpt@gmail.com>
2018-12-18 14:55:51 +0100

    .

---
+++ docs/precache-manifest.cf572bac003e2c74ef313210ba84b245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4185c3c4656278337854",
+    "url": "/tigger/static/js/runtime~main.4185c3c4.js"
+  },
+  {
+    "revision": "1ef702c3706976f71095",
+    "url": "/tigger/static/js/main.1ef702c3.chunk.js"
+  },
+  {
+    "revision": "4b6379eb85687591694d",
+    "url": "/tigger/static/js/1.4b6379eb.chunk.js"
+  },
+  {
+    "revision": "1ef702c3706976f71095",
+    "url": "/tigger/static/css/main.fcf3fa7d.chunk.css"
+  },
+  {
+    "revision": "2fbc232008e6f2d199c5fa6a5eac5972",
+    "url": "/tigger/index.html"
+  }
+];
